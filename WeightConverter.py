e63682ec32d6ce1dc23ee093cce9affc7e0c86c5
Meera Harish <142784753+meeraharish@users.noreply.github.com>
2025-07-24 09:13:51 +0530

    Delete WeightConverter.py

--- WeightConverter.py
+++
@@ -1,9 +0,0 @@
-#mosh 
-input_weight=float(input('Weight: '))
-input_unit=input('(L)bs or (K)g: ')
-if input_unit=='L' or input_unit=='l':
-    kg=input_weight*0.45
-    print(f'you are {kg} kgs')
-elif input_unit=='K' or input_unit=='k':
-    pounds=input_weight/0.45
-    print(f'you are {pounds} pounds ')
